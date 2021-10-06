794d1757c3b452afc4e6cf4b2b62869b0ca25a70
JexLau <jexlau@qq.com>
2021-10-06 20:37:27 +0800

    feat: 修改ci

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,14 @@
 set -e
  
 # 生成静态文件 
-# yarn build
+yarn build
  
 # 进入生成的文件夹
 cd .vitepress/dist 
  
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
